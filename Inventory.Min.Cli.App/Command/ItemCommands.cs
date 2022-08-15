518f8bbbdaa90534abdfd776cd9dc12e9fb72c69
krzm <kmazanek@gmail.com>
2022-08-15 15:36:32 +0200

    Add read cmd

--- Inventory.Min.Cli.App/Command/ItemCommands.cs
+++ Inventory.Min.Cli.App/Command/ItemCommands.cs
@@ -10,20 +10,29 @@ public class ItemCommands
     : Commands
 {
     private const string MainCommand = "item";
-
+    private readonly IReadCommand<ItemFilterArgs> read;
     private readonly IInsertCommand<ItemInsertArgs> insert;
 
     public ItemCommands(
-        IInsertCommand<ItemInsertArgs> insert
+        IReadCommand<ItemFilterArgs> read
+        , IInsertCommand<ItemInsertArgs> insert
         , IConfigReader config)
             : base(config)
     {
+        this.read = read;
         this.insert = insert;
+    }
+
+    [DefaultCommand()]
+    public void Read(ItemFilterArgs model)
+    {
+        read.Read(model);
     }
 
     [Command(InsertCmd)]
     public void Insert(ItemInsertArgs model)
     {
         insert.Insert(model);
+        ReadAfterChange(GetReadTask(read, new ItemFilterArgs()));
     }
 }
