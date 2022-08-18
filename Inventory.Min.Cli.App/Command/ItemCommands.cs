7031fbf916c27536140dff6e64e2df7092e33297
krzm <kmazanek@gmail.com>
2022-08-18 14:08:06 +0200

    add upd cmd

--- Inventory.Min.Cli.App/Command/ItemCommands.cs
+++ Inventory.Min.Cli.App/Command/ItemCommands.cs
@@ -12,15 +12,18 @@ public class ItemCommands
     private const string MainCommand = "item";
     private readonly IReadCommand<ItemFilterArgs> read;
     private readonly IInsertCommand<ItemInsertArgs> insert;
+    private readonly IUpdateCommand<ItemUpdateArgs> update;
 
     public ItemCommands(
         IReadCommand<ItemFilterArgs> read
         , IInsertCommand<ItemInsertArgs> insert
+        , IUpdateCommand<ItemUpdateArgs> update
         , IConfigReader config)
             : base(config)
     {
         this.read = read;
         this.insert = insert;
+        this.update = update;
     }
 
     [DefaultCommand()]
@@ -33,6 +36,13 @@ public class ItemCommands
     public void Insert(ItemInsertArgs model)
     {
         insert.Insert(model);
+        ReadAfterChange(GetReadTask(read, new ItemFilterArgs()));
+    }
+
+    [Command(UpdateCmd)]
+    public void Update(ItemUpdateArgs model)
+    {
+        update.Update(model);
         ReadAfterChange(GetReadTask(read, new ItemFilterArgs()));
     }
 }
