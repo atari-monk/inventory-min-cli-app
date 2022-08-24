ffe7ef8b46e3c646e8782d3d12b62f3e3807d1b6
krzm <kmazanek@gmail.com>
2022-08-24 18:55:23 +0200

    Add parent column test

--- Inventory.Min.Cli.App.Tests/ItemTests/ReadDefaultItemTableTests.cs
+++ Inventory.Min.Cli.App.Tests/ItemTests/ReadDefaultItemTableTests.cs
@@ -38,12 +38,41 @@ public class ReadDefaultItemTableTests
     }
 
     [Theory]
+    [MemberData(nameof(ReadDefaultItemTableData.Insert02)
+        , MemberType= typeof(ReadDefaultItemTableData))]
+    public void Test02(int index, Item expected, string[] cmd)
+    {
+        fixture.AssertItemCount(fixture.Uow, index);
+        var parent = fixture.GetItem(fixture.Uow, 0);
+        var command = new List<string>(cmd);
+        SetValue(command, "parentId", parent.Id.ToString());
+        fixture.RunCmd(fixture.Booter, command.ToArray());
+        fixture.AssertItemCount(fixture.Uow, index + 1);
+        var actual = fixture.GetItem(fixture.Uow, index);
+        expected.ParentId = parent.Id;
+        fixture.AssertItem(expected, actual);
+    }
+
+    private void SetValue(
+        List<string> cmd
+        , string key
+        , string value)
+    {
+        cmd[GetIndex(cmd, key)] = value;
+    }
+
+    private int GetIndex(List<string> cmd, string value)
+    {
+        return cmd.IndexOf(value);
+    }
+
+    [Theory]
     [MemberData(nameof(ReadDefaultItemTableData.Read01)
         , MemberType= typeof(ReadDefaultItemTableData))]
-    public void Test02(int index, string[] cmd, string expected)
+    public void Test03(int index, string[] cmd, string expected)
     {
+        fixture.AssertItemCount(fixture.Uow, index + 1);
         fixture.RunCmd(fixture.Booter, cmd);
-        var item = fixture.GetItem(fixture.Uow, 0);
         var logger = fixture.Booter.GetLogger();
         logger
             .Should()
@@ -55,13 +84,12 @@ public class ReadDefaultItemTableTests
             .WithProperty("1")
             .WithValue("Item");
         var output = (IOutMock)fixture.Booter.GetOut();
-        fixture.AssertItemCount(fixture.Uow, index + 1);
         var outputText = output.OutText;
         var linesOut = outputText!.Split(EOL).ToList();
         var length = linesOut[0].IndexOf("┐") + 1;
         linesOut[0] = linesOut[0].Substring(0, length);
         linesOut[2] = linesOut[2].Substring(0, length);
-        linesOut[4] = linesOut[4].Substring(0, length);
+        linesOut[5] = linesOut[5].Substring(0, length);
         PrintToFile(expected, linesOut, true);
         outputText = string.Join(EOL, linesOut);
         Assert.Equal(expected, outputText);
