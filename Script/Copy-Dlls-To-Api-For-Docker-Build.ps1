802471482c252df6eb761ee62b81a5c28533e65f
krzm <kmazanek@gmail.com>
2022-08-26 17:17:06 +0200

    Remve api proj to other sln

--- Script/Copy-Dlls-To-Api-For-Docker-Build.ps1
+++
@@ -1,9 +0,0 @@
-$path = "C:\kmazanek.gmail.com\Code\inventory-min-cli-app\Inventory.Min.Api\dlls"
-if (-not (Test-Path $path))
-{
-New-Item -Path "C:\kmazanek.gmail.com\Code\inventory-min-cli-app\Inventory.Min.Api" -Name "dlls" -ItemType "directory"
-}
-Copy-Item -Path "C:\kmazanek.gmail.com\Build\efcore-helper\EFCore.Helper\EFCore.Helper.dll" -Destination $path
-Copy-Item -Path "C:\kmazanek.gmail.com\Build\model-helper\ModelHelper\ModelHelper.dll" -Destination $path
-Copy-Item -Path "C:\kmazanek.gmail.com\Build\inventory-min-data\Inventory.Min.Data\Inventory.Min.Data.dll" -Destination $path
-Copy-Item -Path "C:\kmazanek.gmail.com\Build\inventory-min-lib\Inventory.Min.Lib\Inventory.Min.Lib.dll" -Destination $path
