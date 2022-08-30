06a2c24cc6e5959625f27a1ee1da4a8c71764ebe
krzm <kmazanek@gmail.com>
2022-08-30 17:00:45 +0200

    Fix ins tests

--- Inventory.Min.Cli.App.Tests/ItemTests/InsertTests.cs
+++ Inventory.Min.Cli.App.Tests/ItemTests/InsertTests.cs
@@ -28,40 +28,39 @@ public class InsertTests
         var actual = fixture.GetItem(fixture.Uow, index);
         fixture.AssertItem(expected, actual);
     }
+   
+    [Theory]
+    [MemberData(nameof(InsertData.Insert02)
+        , MemberType= typeof(InsertData))]
+    public void Test02(int index, Item expected, string[] cmd)
+    {
+        fixture.AssertItemCount(fixture.Uow, index);
+        var parent = fixture.GetItem(fixture.Uow, elementIndex: 0);
+        var command = new List<string>(cmd);
+        SetValue(command, "parentId", parent.Id.ToString());
+        fixture.RunCmd(fixture.Booter, command.ToArray());
+        fixture.AssertItemCount(fixture.Uow, index + 1);
+        var actual = fixture.GetItem(fixture.Uow, index);
+        expected.ParentId = parent.Id;
+        fixture.AssertItem(expected, actual);
+    }
 
     [Fact]
-    public void Test02()
+    public void Test03()
     {
         var seeder = new TestSeeder();
         seeder.Seed();
     }
 
     [Theory]
-    [MemberData(nameof(InsertData.Insert02)
+    [MemberData(nameof(InsertData.Insert03)
         , MemberType= typeof(InsertData))]
-    public void Test03(int index, Item expected, string[] cmd)
+    public void Test04(int index, Item expected, string[] cmd)
     {
         fixture.AssertItemCount(fixture.Uow, index);
         fixture.RunCmd(fixture.Booter, cmd);
         fixture.AssertItemCount(fixture.Uow, index + 1);
         var actual = fixture.GetItem(fixture.Uow, index);
-        fixture.AssertItem(expected, actual);
-    }
-
-    [Theory]
-    [MemberData(nameof(InsertData.Insert03)
-        , MemberType= typeof(InsertData))]
-    public void Test04(Item expected, string[] cmd)
-    {
-        const int PrevTestInsertCount = 21;
-        fixture.AssertItemCount(fixture.Uow, PrevTestInsertCount);
-        var parent = fixture.GetItem(fixture.Uow, elementIndex: PrevTestInsertCount - 1);
-        var command = new List<string>(cmd);
-        SetValue(command, "parentId", parent.Id.ToString());
-        fixture.RunCmd(fixture.Booter, command.ToArray());
-        fixture.AssertItemCount(fixture.Uow, PrevTestInsertCount + 1);
-        var actual = fixture.GetItem(fixture.Uow, PrevTestInsertCount);
-        expected.ParentId = parent.Id;
         fixture.AssertItem(expected, actual);
     }    
 }
